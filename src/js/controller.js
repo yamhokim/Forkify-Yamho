4b513267f767e266128509cfb731f0e11cac387b
yamhokim <yoonho.luke.kim@gmail.com>
2024-06-26 18:49:10 -0400

    cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,10 +118,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to Forkify!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,7 +126,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerFormSubmission(controlAddRecipe);
-  newFeature();
 };
 
 init();
